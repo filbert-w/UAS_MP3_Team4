3848270b66f4e6432e9438a4a27f18a6c28ee699
filbert-w <114629987+filbert-w@users.noreply.github.com>
2023-12-26 16:10:16 +0700

    Perbaikan

--- app/src/main/java/com/example/projectmovie/modeldata/SearchData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.projectmovie.modeldata
-
-import com.google.gson.annotations.SerializedName
-
-data class SearchData(
-    @SerializedName("Search") val data:List<MovieData>,
-    @SerializedName("totalResults") val totalData:Int,
-    @SerializedName("Response") val res:String
-)
